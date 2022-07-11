fdd9a4a645c07a654879c8105f3e319bfa40d6b4
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2022-07-11 18:06:58 -0300

    Componente com propriedade

---
+++ src/components/basicos/ComParametro.jsx
@@ -0,0 +1,12 @@
+export default function ComParametro(props) {
+  const status = props.nota >= 7 ? "Aprovado" : "Em recuperação";
+  return (
+    <div>
+      <h2>{props.titulo}</h2>
+      <p>
+        <strong>{props.aluno}</strong> tem nota <strong>{props.nota}</strong>
+        e está { status }
+      </p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
-import Primeiro from './components/basicos/Primeiro';
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <div>
-        <Primeiro></Primeiro>
-    </div>
+  <div>
+    <Primeiro />
+    <ComParametro titulo="Situação do Aluno" aluno="Wendell" nota={9.3} />
+  </div>
 );
