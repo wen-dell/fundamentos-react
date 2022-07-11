144b1cce0ba7e6a80f1626317eefb06b591e1b43
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2022-07-11 13:03:03 -0300

    Hello World customizado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<div>Olá React!</div>); 
-    
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <div>
+        <strong>Olá React!</strong>
+    </div>
+);
