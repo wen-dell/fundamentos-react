b273314610a1e62cb6a8151b29ea4c2e671e1192
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2022-07-11 21:27:13 -0300

    Usando React.Fragment

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <div>
     <Primeiro />
     <ComParametro titulo="Situação do Aluno" aluno="Wendell" nota={9.3} />
+    <Fragmento />
   </div>
 );
