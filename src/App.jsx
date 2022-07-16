bcfe27d17d3e73be17806c3763ae1c3734c53f0b
Wendell Mccaulay Ramos Alves <wendellramos.gft@sulamerica.com.br>
2022-07-16 09:02:48 -0300

    Componente Card #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
@@ -5,24 +6,26 @@ import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/Primeiro";
 
 const App = (props) => (
-  <div>
+  <div className="App">
     <h1>Fundamentos React</h1>
 
-    <Card titulo="#04 - Desafio Aleatório">
-      <Aleatorio min={1} max={10}/>
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={10} />
+      </Card>
 
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
 
-    <Card titulo="#02 - Com Parâmetro">
-      <ComParametro titulo="Situação do Aluno" aluno="Wendell" nota={9.3} />
-    </Card>
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro titulo="Situação do Aluno" aluno="Wendell" nota={9.3} />
+      </Card>
 
-    <Card titulo="#01 - Primeiro Componente">
-      <Primeiro />
-    </Card>
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
 
